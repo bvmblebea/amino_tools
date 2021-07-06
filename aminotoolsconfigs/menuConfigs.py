73d9800dbbfdaade646bc0ea0ba9be306b8c68f0
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-07-06 17:43:51 +0600

    menu Config

---
+++ aminotoolsconfigs/menuConfigs.py
@@ -0,0 +1,51 @@
+def mainmenu():
+	print("""[1] Amino-Tools/spamTools
+[2] Amino-Tools/chatTools
+[3] Amino-Tools/activityTools
+[4] Amino-Tools/profileTools
+[5] Amino-Tools/raidTools
+[6] Amino-Tools/Don't Click!
+[0] Amino-Tools/Exit""")
+
+def spamtoolsmenu():
+	print("""[1] Amino-Tools/SpamBot
+[2] Amino-Tools/WikiSpamBot
+[3] Amino-Tools/WallSpamBot
+[4] Amino-Tools/BlogCommentSpamBot
+[0] Amino-Tools/Exit""")
+
+def chattoolsmenu():
+	print("""[1] Amino-Tools/ChatIdFinder
+[2] Amino-Tools/ChatSecurity
+[3] Amino-Tools/Co-HostKick
+[4] Amino-Tools/FakeCoinTransaction
+[0] Amino-Tools/Exit""")
+
+def activitytoolsmenu():
+	print("""[1] Amino-Tools/InviteBot
+[2] Amino-Tools/LikeBot
+[3] Amino-Tools/FollowBot
+[4] Amino-Tools/UnfollowBot
+[0] Amino-Tools/Exit""")
+
+def profiletoolsmenu():
+	print("""[1] Amino-Tools/AntiBan
+[2] Amino-Tools/PostSpam
+[3] Amino-Tools/WikiSpam
+[0] Amino-Tools/Exit""")
+
+def raidtoolsmenu():
+	print("""[1] Amino-Tools/SystemMessageSpam
+[2] Amino-Tools/SendSystemMessage
+[3] Amino-Tools/Join&LeaveSpam
+[4] Amino-Tools/ChatCrash
+[5] Amino-Tools/CoinIpRecipient
+[6] Amino-Tools/AminoAdvBo
+[7] Amino-Tools/JoinActiveChats
+[8] Amino-Tools/AutoReg
+[0] Amino-Tools/Exit""")
+
+def dontclickmenu():
+	print("Wow U Clicked ^-^")
+	print("""[1] Amino-Tools/DeviceIdGenerator
+[2] Amino-Tools/CoinGenerator""")
