0369a6849db507c2310624bdd00418c64f626cf7
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-08-08 22:20:39 +0600

    Update menuConfigs.py

--- aminotoolsconfigs/menuConfigs.py
+++ aminotoolsconfigs/menuConfigs.py
@@ -1,11 +1,14 @@
+import terminal_banner
+
 def mainmenu():
-	print("""[1] Amino-Tools/spamTools
+	print(terminal_banner.Banner("""
+[1] Amino-Tools/spamTools
 [2] Amino-Tools/chatTools
 [3] Amino-Tools/activityTools
 [4] Amino-Tools/profileTools
 [5] Amino-Tools/raidTools
-[6] Amino-Tools/Don't Click!
-[0] Amino-Tools/Exit""")
+[0] Amino-Tools/Exit
+"""))
 
 def spamtoolsmenu():
 	print("""[1] Amino-Tools/SpamBot
@@ -38,14 +41,6 @@ def raidtoolsmenu():
 	print("""[1] Amino-Tools/SystemMessageSpam
 [2] Amino-Tools/SendSystemMessage
 [3] Amino-Tools/Join&LeaveSpam
-[4] Amino-Tools/ChatCrash
-[5] Amino-Tools/CoinIpRecipient
-[6] Amino-Tools/AminoAdvBo
-[7] Amino-Tools/JoinActiveChats
-[8] Amino-Tools/AutoReg
+[4] Amino-Tools/CoinIpRecipient
+[5] Amino-Tools/JoinActiveChats
 [0] Amino-Tools/Exit""")
-
-def dontclickmenu():
-	print("Wow U Clicked ^-^")
-	print("""[1] Amino-Tools/DeviceIdGenerator
-[2] Amino-Tools/CoinGenerator""")
