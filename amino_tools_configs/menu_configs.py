7d4451610d73ef253346f5cf1af03556f17eb690
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-01-24 14:18:15 +0600

    Update menu_configs.py

--- amino_tools_configs/menu_configs.py
+++ amino_tools_configs/menu_configs.py
@@ -12,29 +12,25 @@ spam_tools_menu = [
 	["1", "Spam Bot", "Amino-Tools"],
 	["2", "Wiki Spam Bot"],
 	["3", "Wall Spam Bot"],
-	["4", "Blog Spam Bot"],
-	["0", "Exit"]
+	["4", "Blog Spam Bot"]
 ]
 
 chat_tools_menu = [
 	["1", "ChatId Finder", "Amino-Tools"],
 	["2", "Crash Chat Description"],
-	["3", "Transfer Fake Coins"],
-	["0", "Exit"]
+	["3", "Transfer Fake Coins"]
 ]
 
 activity_tools_menu = [
 	["1", "Invite Bot", "Amino-Tools"],
 	["2", "Like Bot"],
 	["3", "Follow Bot"],
-	["4", "Unfollow Bot"],
-	["0", "Exit"]
+	["4", "Unfollow Bot"]
 ]
 
 profile_tools_menu = [
 	["1", "Blogs Spam Bot", "Amino-Tools"],
-	["2", "Wiki Spam Bot"],
-	["0", "Exit"]
+	["2", "Wiki Spam Bot"]
 ]
 
 
@@ -42,12 +38,10 @@ raid_tools_menu = [
 	["1", "Spam System Messages", "Amino-Tools"],
 	["2", "Send System Message"],
 	["3", "Spam With Join And Leave"],
-	["4", "Coin Ip Recipient"],
-	["5", "Join Active Chats"],
-	["0", "Exit"]
+	["4", "Join Active Chats"]
 ]
 
-chat_Id_finder_menu = [
+chat_id_finder_menu = [
 	["1", "Get Public Chats ChatId", "Amino-Tools"],
 	["2", "Get Joined Chats ChatId"]
 ]
