184acf53454c941fee714b80639de3f52d932fa8
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-02-10 09:51:56 +0600

    Update Amino-Tools.py

--- Amino-Tools.py
+++ Amino-Tools.py
@@ -1,11 +1,12 @@
 from sty import fg
-from pyfiglet import figlet_format
 from asyncio import get_event_loop
 from amino_tools_configs import main_functions
-print(
-    f"""{fg(36)}Script by deluvsushi
-Github : https://github.com/deluvsushi"""
-)
-print(figlet_format("Amino-Tools", font="smslant", width=58))
+print(f"""{fg(36)}
+Script by deluvsushi
+Github : https://github.com/deluvsushi
+
+▄▀█ █▀▄▀█ █ █▄░█ █▀█ ▄▄ ▀█▀ █▀█ █▀█ █░░ █▀
+█▀█ █░▀░█ █ █░▀█ █▄█ ░░ ░█░ █▄█ █▄█ █▄▄ ▄█
+""")
 
 get_event_loop().run_until_complete(main_functions.main())
