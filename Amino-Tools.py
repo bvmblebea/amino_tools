53465517f789418d06494c5f292dac9b7583aef2
zeviel <77536370+zeviel@users.noreply.github.com>
2022-08-25 15:20:22 +0800

    Update Amino-Tools.py

--- Amino-Tools.py
+++ Amino-Tools.py
@@ -2,8 +2,8 @@ from sty import fg
 from asyncio import get_event_loop
 from amino_tools_configs import main_functions
 print(f"""{fg(36)}
-Script by deluvsushi
-Github : https://github.com/deluvsushi
+Script by zeviel
+Github : https://github.com/zeviel
 ▄▀█ █▀▄▀█ █ █▄░█ █▀█ ▄▄ ▀█▀ █▀█ █▀█ █░░ █▀
 █▀█ █░▀░█ █ █░▀█ █▄█ ░░ ░█░ █▄█ █▄█ █▄▄ ▄█
 """)
